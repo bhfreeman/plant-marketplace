fca0d5b6c179904f80f2fad505e5b21c3d468586
grey cashmere <dobsonemily@gmail.com>
2021-04-30 13:23:25 -0700

    working on navbar

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+function Footer() {
+    return (
+        <div>
+    <footer className="footer">
+  <div class="content has-text-centered">
+    <p>
+      <strong>Re-Potted</strong> by <a href="https://jgthms.com">Brett, Kyle and Emily, 2021</a>
+      <a href="#">Github</a>
+    </p>
+  </div>
+</footer>  
+        </div>
+    )
+}
+
+export default Footer; 
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,15 +2,45 @@ import React from "react";
 // import { useStoreContext } from "../../utils/GlobalState";
 
 function Nav() {
-  const [store] = useStoreContext();
+  // const [store] = useStoreContext();
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/">
-        Your personal CMS
-      </a>
-      {store.loading ? <a className="navbar-brand ml-auto">Loading...</a> : <></>}
+    <nav className="navbar my-navbar">
+      <div className="navbar-brand">
+        <div className="navbar-burger burger" data-target="my-navbar-menu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+
+      <div id="my-navbar-menu" className="navbar-menu">
+        <div className="navbar-start">
+          <div className="buttons">
+            <a className="button is-primary">
+                  <strong>Log in</strong>
+            </a>
+            <a class="button is-light">
+                  <strong>View account</strong>
+            </a>
+
+            <div id="my-navbar-center" className= "navbar-center">
+            <a className="navbar-item"href="/" >
+            <h1 className ="title is-2">RePotted</h1>     
+           </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="navbar-end">
+        <div className="navbar-item">
+          <a href="#">Home</a>
+        </div>
+      </div>
+      </div>
+    
     </nav>
+
   );
 }
 
