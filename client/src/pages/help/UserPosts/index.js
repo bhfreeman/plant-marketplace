3d1f4d2c2d0632ffa09ef46a22204850937f7373
grey cashmere <dobsonemily@gmail.com>
2021-05-01 16:31:37 -0700

    adding additional files

--- client/src/pages/help/UserPosts/index.js
+++ client/src/pages/help/UserPosts/index.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from "react";
 import { ListItem, List } from "../List";
-import DeleteBtn from "../DeleteBtn";
+import DeleteBtn from "../../../components/DeleteBtn";
 import { Link } from "react-router-dom";
 // import { useStoreContext } from "../../utils/GlobalState";
 // import { REMOVE_POST, UPDATE_POSTS, LOADING } from "../../utils/actions";
-import API from "../../utils/API";
+import API from "../../../utils/API";
 
 function PostsList() {
   const [state, dispatch] = useStoreContext();
