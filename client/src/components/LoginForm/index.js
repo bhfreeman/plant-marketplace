2ab76506f43deb40a81abda1cf97a230f40fbc2f
grey cashmere <dobsonemily@gmail.com>
2021-05-01 16:52:27 -0700

    form for login done

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -1,10 +1,26 @@
 import React from 'react'
 
 function LoginForm() {
-    return (
-        <div>
-            
-        </div>
+    return(
+        <section>
+          <div class="field">
+  <p class="control has-icons-left has-icons-right">
+    <input class="input" type="userName" placeholder="userName"/>
+  </p>
+</div>
+<div class="field">
+  <p class="control has-icons-left">
+    <input class="input" type="password" placeholder="Password"/>
+  </p>
+</div>
+<div class="field">
+  <p class="control">
+    <button class="button is-success is-inverted">
+      Login
+    </button>
+  </p>
+</div>  
+</section>
     )
 }
 
