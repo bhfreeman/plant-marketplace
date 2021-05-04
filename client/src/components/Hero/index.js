db641278e0fcde0fb438c52b446ca7c2d0b605da
grey cashmere <dobsonemily@gmail.com>
2021-05-04 14:00:15 -0700

    hero image adjustment

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -7,22 +7,29 @@ import React from "react";
 
 function Hero() {
   return (
-    <div class="container">
+    <div class="container-flex">
     <section class="hero">
-    <div class="hero-body">
-      <figure class= "background image" style= {{backgroundImage: "linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.5))",  
+    <div class="hero-body" style={{ width:"100%",  
+       height: "75%", padding: 0, margin: "0%" }}>
+    
+      <figure class= "background image" style= {{backgroundImage: "linear-gradient(rgba(0,0,1,10),rgba(0,0,0,0.5))",  
        backgroundPosition: "center", 
        backgroundRepeat: "noRepeat",
-       backgroundSize: "cover",  
-       height: "80%", 
+       backgroundSize: "cover",
+       width:"100",   
        display:"flex" }}>
         <img src= "/images/hero.jpg" 
         alt= "cactus on dark background"/>
         </figure>
-      <h1 class="title has-text-white" style= {{textAlign: "center", fontSize: "40px"}}>
+        <h1 class="title" style= {{textAlign: "center", fontSize: "55px", color:"#f5f5f5",  
+      position:"absolute", 
+    top: "50%",
+    left: "30%",}}>
        Repotted
       </h1>
-      <h2 class="subtitle has-text-white">
+      <h2 class="subtitle"style= {{color: "#f5f5f5", textAlign: "center", top: "70%",
+    left: "20%",   position:"absolute", 
+   }}>
        Welcome to a plant marketplace
       </h2>
     </div>
