4ba51b4389c956a9532757d6550a8c1c9d3d1860
grey cashmere <dobsonemily@gmail.com>
2021-05-04 14:18:24 -0700

    hero updated

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -7,27 +7,27 @@ import React from "react";
 
 function Hero() {
   return (
-    <div class="container-flex">
-    <section class="hero">
+  <div className="container-flex">
+    <section className="hero">
     <div class="hero-body" style={{ width:"100%",  
-       height: "75%", padding: 0, margin: "0%" }}>
+       padding: 0, margin: "0%" }}>
     
-      <figure class= "background image" style= {{backgroundImage: "linear-gradient(rgba(0,0,1,10),rgba(0,0,0,0.5))",  
+      <figure className= "background image" style= {{backgroundImage: "linear-gradient(rgba(0,0,1,10),rgba(0,0,0,0.5))",  
        backgroundPosition: "center", 
        backgroundRepeat: "noRepeat",
        backgroundSize: "cover",
        width:"100",   
-       display:"flex" }}>
+       display:"flex", height: "75%"}}>
         <img src= "/images/hero.jpg" 
         alt= "cactus on dark background"/>
         </figure>
-        <h1 class="title" style= {{textAlign: "center", fontSize: "55px", color:"#f5f5f5",  
+        <h1 className="title" style= {{textAlign: "center", fontSize: "55px", color:"#f5f5f5",  
       position:"absolute", 
     top: "50%",
     left: "30%",}}>
        Repotted
       </h1>
-      <h2 class="subtitle"style= {{color: "#f5f5f5", textAlign: "center", top: "70%",
+      <h2 className="subtitle"style= {{color: "#f5f5f5", textAlign: "center", top: "70%",
     left: "20%",   position:"absolute", 
    }}>
        Welcome to a plant marketplace
@@ -40,3 +40,6 @@ function Hero() {
 
 export default Hero;
 
+// style={{minHeight: "200px",
+//     height: "60vw",
+//     maxHeight: "500px"}}
