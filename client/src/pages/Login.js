3f8c2b4e99ba7eeb1c73eaaa51f43695fac26ae3
bhfreeman <brett.h.freeman@gmail.com>
2021-05-05 11:10:21 -0700

    starting api calls on login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import SignupForm from "../components/SignupForm";
 import LoginForm from "../components/LoginForm";
 
 function Login() {
+  const [userName, setuserName] = useState('')
+  const [userPassword, setUserPassword] = useState('')
+
+  function handleLogin() {
+
+  }
+
+  function handleSignup(){
+    
+  }
+
   return (
     <div className="login-signup-page">
       <div className="sign-up">
