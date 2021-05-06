bed34655f1ac3bb9aa11d90a70a5d9dcc4fcee47
Kyle Grabski <kyle.grabski@gmail.com>
2021-05-06 10:24:35 -0700

    adjust styling RECENT POSTS text

--- client/src/pages/LandingPg.js
+++ client/src/pages/LandingPg.js
@@ -9,17 +9,18 @@ function LandingPg() {
     <div>
       <Hero />
       {/* map over the example data to create multiple example posts, pass in each item in the example data as props. */}
-        <h1 className="title is-size-3"
-        style={{marginTop:"20px", width:"50vmax", textAlign:"center"}}
-        > Recent Posts</h1>
+      <h1
+        className="title is-size-3"
+        style={{ marginTop: "20px", width: "50vmax", textAlign: "center" }}
+      >
+        {" "}
+        Recent Posts
+      </h1>
       <div className="home-page columns">
         <div
           className="home-page-posts column is-half is-5 is-flex is-flex-wrap-wrap is-justify-content-space-around"
           style={{ borderRight: "solid", margin: "15px 50px" }}
         >
-
-
-
           {exampleData.map((item) => (
             <HomePagePosts key={item.id} {...item} className="card  " />
           ))}
