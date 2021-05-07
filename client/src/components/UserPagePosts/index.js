8fa6d208b7afc77dc9d6c39732b4fbec993e8cb4
bhfreeman <brett.h.freeman@gmail.com>
2021-05-07 11:12:55 -0700

    commit to merge

--- client/src/components/UserPagePosts/index.js
+++ client/src/components/UserPagePosts/index.js
@@ -14,7 +14,7 @@ function UserPagePosts({ posts, user }) {
             <div key={post._id}>
               <BaseSalesPost
                 key={post._id}
-                plantName={post.plant_name}
+                plant_name={post.plant_name}
                 description={post.description}
                 image_link={post.image_link}
                 username={user.username}
