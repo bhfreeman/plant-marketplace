c51c85bbd642f4d2ec1f9782b575f8d362a63536
Kyle Grabski <kyle.grabski@gmail.com>
2021-05-07 12:53:11 -0700

    refactor/add username

--- client/src/components/HomePagePosts/index.js
+++ client/src/components/HomePagePosts/index.js
@@ -3,37 +3,56 @@ import React from "react";
 //used a box to give it a bit of emphasis
 
 // passing in props from LandingPg.js
-function HomePagePosts({ plant_name, image_link, description, contact_email }) {
+function HomePagePosts({ plant_name, image_link, description, email, username }) {
   return (
     // flex box with margin and padding so two can fit side by side
-    <div className="is-flex
-    is-flex-direction-column" 
-    // className={styles.cardContent}
-    style={{ maxWidth: "250px", minHeight: "300px", marginBottom:"2em", border:"black 1px dashed", borderRadius:"8px", padding:"1rem",  boxShadow:" 0 10px 20px rgba(0,0,0,.4)"}}>
-
+    <div
+      className="is-flex
+    is-flex-direction-column"
+      // className={styles.cardContent}
+      style={{
+        maxWidth: "250px",
+        minHeight: "300px",
+        marginBottom: "2em",
+        border: "black 1px dashed",
+        borderRadius: "8px",
+        padding: "1rem",
+        boxShadow: " 0 10px 20px rgba(0,0,0,.4)",
+      }}
+    >
       <div className="card-image ">
         <figure className="image is-4by3">
           <img src={image_link} alt="Image" />
         </figure>
       </div>
-        <div className="content is-flex is-flex-direction-column"
-        style={{height:"100%"}}>
+      <div
+        className="content is-flex is-flex-direction-column"
+        style={{ height: "100%" }}
+      >
+        <p>
+          <strong>
+            <em>Plant Name: </em>
+            {plant_name}
+          </strong>
+          <br></br>
+          {description}
+        </p>
+        <div
+          className="content"
+          style={{
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-end",
+          }}
+        >
           <p>
-            <strong>
-              <em>Plant Name: </em>
-              {plant_name}
-            </strong>
-            <br></br>
-            {description}
+            Contact Seller:
+            <a href={`mailto:${email}`}> {username}</a>
           </p>
-          <div
-            className="content"
-            style={{ height:"100%", display:"flex", flexDirection:"column", justifyContent:"flex-end" }}>
-            <p className="is-size-6">{}</p>
-            <a href={contact_email}>Contact Seller</a>
-          </div>
         </div>
       </div>
+    </div>
   );
 }
 
